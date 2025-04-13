c92323b788ff25cd11c0273fafe0e8ba295f76cc
Reevlon <agustinreevlon@gmail.com>
2025-04-13 17:58:47 +0800

    Add entry point file

--- App.js
+++ App.js
@@ -1,4 +1,2 @@
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
